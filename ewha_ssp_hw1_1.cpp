8633136a97a67e627e01e42de64ca175ca1b700c
unknown <yjin3012@hanmail.net>
2015-03-25 19:54:40 +0900

    Started hw1-1 program

--- ewha_ssp_hw1_1.cpp
+++ ewha_ssp_hw1_1.cpp
@@ -5,20 +5,30 @@
  * GitHub Setup Homework 
  * Instructor: Prof. HyungJune Lee
  *
- * Coded by Firstname Lastname
- * Mar xx, 2015
+ * Coded by JinYi Yoon
+ * Mar 25, 2015
  *
  */
 
 // Add #define pre-processor below
+#include "Lab1_1.h"
 
+int Front_LED = 10;
+int Back_LED = 9;
  
 void setup() {
   //Add your code of Lab Assignment #1
-
+	pinMode(Front_LED, OUTPUT);
+	pinMode(Back_LED, OUTPUT);
 }
 
 void loop() {
   //Add your code of Lab Assignment #1  
+	digitalWrite(Front_LED, HIGH);
+	digitalWrite(Back_LED, LOW);
+	delay(1000);
 
+	digitalWrite(Front_LED, LOW);
+	digitalWrite(Back_LED, HIGH);
+	delay(1000);
 }
